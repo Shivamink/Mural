7641bdd0f22e9b46ab04d246c66427856cc5d26d
Shivamink <102367658+Shivamink@users.noreply.github.com>
2022-06-30 12:45:42 +0530

    Carousel changes

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -6,7 +6,6 @@ import "./Carousel.css";
 function MyCarousel({ onBackDropClick, type, urls, itemTop, itemLeft }) {
   useEffect(() => {
     console.log({ type, urls, itemTop, itemLeft });
-    // console.log(itemTop, itemLeft);
     // if (itemTop > 700) {
     //   document.getElementById("my-carousel").style.top = `${itemTop}px`;
     //   document.getElementById("my-carousel").style.left = `${itemLeft + 500}px`;
@@ -16,15 +15,40 @@ function MyCarousel({ onBackDropClick, type, urls, itemTop, itemLeft }) {
     // }
   }, []);
 
+  const [autoplay, Setautoplay] = useState(false);
+  const videoRef = useRef(null);
+  let carouselarray = document.querySelectorAll(
+    ".carousel-inner .carousel-item"
+  );
+  const handleSelect = (selectedIndex, e) => {
+    carouselarray.forEach((random) => {
+      console.log(random.classList.value === "active carousel-item");
+
+      if (random.classList.value === "active carousel-item") {
+        random.childNodes[0].pause();
+      } else {
+        random.childNodes[0].play();
+      }
+    });
+  };
+
   return (
     <div id="my-carousel-wrapper">
       <div id="carousel-backdrop" onClick={onBackDropClick}></div>
       <div id="my-carousel" className="ratio ratio-16x9">
-        <Carousel interval={null}>
+        <Carousel interval={null} onSelect={handleSelect}>
           {urls.map((url, i) => (
             <Carousel.Item key={i}>
               {type === "video" && (
-                <video src={url} autoPlay muted loop controls></video>
+                <video
+                  ref={videoRef}
+                  id="player"
+                  src={url}
+                  loop
+                  controls
+                  autoPlay="autoplay"
+                  // autoPlay={autoplay}
+                ></video>
               )}
               {type === "image" && <img src={url} />}
               {type === "text" && typeof url === "string" && (
